da427c89b1a010b95f8353a10ab6afbdbac4f4d4
yulia-piskunova <y.piskunova@tapston.com>
2020-10-18 13:09:29 +0300

    initial files

---
+++ cmake-build-debug/CMakeFiles/CBasics.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CBasics"
+  "CBasics.pdb"
+  "CMakeFiles/CBasics.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CBasics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
